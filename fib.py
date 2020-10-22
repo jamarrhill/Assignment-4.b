186dd1632f6a2d7bb5526ddbfe3808da4a48e34f
jamarrhill <hilljama@oregonstate.edu>
2020-10-21 17:56:00 -0800

    Assignment 4.b

---
+++ fib.py
@@ -0,0 +1,16 @@
+# Name: Jamar Hill
+# Date: 10/21/2020
+# Description: Project 4.b
+
+def fib(n): #Describe "n" as a variable in fib sequence
+   while n == 0:
+       return 0 #establish that 0 position is equal to 0
+   if n == 1:
+       return 1
+   else:
+      return fib(n-1) + fib(n-2)
+
+
+
+term = fib(17)
+print(term)
